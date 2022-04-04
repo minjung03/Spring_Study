8955d3fe8f77cae590b3db66b3a53eaf720594fc
minjung Kim <ikwmjj0625@gmail.com>
2022-04-04 15:03:59 +0900

    4/4 수업 예제 커밋

---
+++ JavaDI/src/main/java/kr/hs/study/beans/DataBean1.java
@@ -0,0 +1,8 @@
+package kr.hs.study.beans;
+
+public class DataBean1 {
+
+	public DataBean1() {
+		
+	}
+}
--- JavaDI/src/main/java/kr/hs/study/config/BeanConfigClass.java
+++ JavaDI/src/main/java/kr/hs/study/config/BeanConfigClass.java
@@ -1,5 +1,27 @@
 package kr.hs.study.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import kr.hs.study.beans.DataBean1;
+import kr.hs.study.beans.TestBean1;
+
+@Configuration
 public class BeanConfigClass {
 
+	
+	@Bean
+	public TestBean1 java1() {
+		TestBean1 t1 = new TestBean1(200, "spring2", new DataBean1()); // java에서의 생성자 DI
+		return t1;
+	}
+	
+	@Bean
+	public TestBean1 java2() {
+		TestBean1 t2 = new TestBean1(); 
+		t2.setData1(300);
+		t2.setData2("spring2");
+		t2.setData3(new DataBean1());
+		return t2;
+	}
 }
