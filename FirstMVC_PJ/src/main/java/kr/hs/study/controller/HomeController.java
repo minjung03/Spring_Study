93531b1eabebf82d2e0ca2b296d5b6d3c500a0dd
minjung Kim <ikwmjj@naver.com>
2022-06-01 20:49:04 +0900

    Add files via upload

---
+++ FirstMVC_PJ/src/main/java/kr/hs/study/controller/HomeController.java
@@ -0,0 +1,15 @@
+package kr.hs.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+}
